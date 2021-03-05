d31501c03320db36f101aaaa67c76fc515a55e00
Allisson Azevedo <allisson@gmail.com>
2021-03-05 16:02:18 -0300

    feat: Add DeliveryService (#13)

---
+++ service/delivery.go
@@ -0,0 +1,55 @@
+package service
+
+import (
+	"context"
+	"time"
+
+	"github.com/allisson/postmand"
+	"github.com/google/uuid"
+)
+
+// Delivery implements postmand.DeliveryService interface.
+type Delivery struct {
+	deliveryRepository postmand.DeliveryRepository
+}
+
+// Get returns postmand.Delivery by options filter.
+func (d Delivery) Get(ctx context.Context, getOptions postmand.RepositoryGetOptions) (*postmand.Delivery, error) {
+	return d.deliveryRepository.Get(ctx, getOptions)
+}
+
+// List returns a slice of postmand.Delivery by options filter.
+func (d Delivery) List(ctx context.Context, listOptions postmand.RepositoryListOptions) ([]*postmand.Delivery, error) {
+	return d.deliveryRepository.List(ctx, listOptions)
+}
+
+// Create postmand.Delivery on database.
+func (d Delivery) Create(ctx context.Context, delivery *postmand.Delivery) error {
+	now := time.Now().UTC()
+	delivery.ID = uuid.New()
+	delivery.CreatedAt = now
+	delivery.UpdatedAt = now
+	return d.deliveryRepository.Create(ctx, delivery)
+}
+
+// Update postmand.Delivery on database.
+func (d Delivery) Update(ctx context.Context, delivery *postmand.Delivery) error {
+	getOptions := postmand.RepositoryGetOptions{Filters: map[string]interface{}{"id": delivery.ID}}
+	storedDelivery, err := d.deliveryRepository.Get(ctx, getOptions)
+	if err != nil {
+		return err
+	}
+	delivery.CreatedAt = storedDelivery.CreatedAt
+	delivery.UpdatedAt = time.Now().UTC()
+	return d.deliveryRepository.Update(ctx, delivery)
+}
+
+// Delete postmand.Delivery on database.
+func (d Delivery) Delete(ctx context.Context, id postmand.ID) error {
+	return d.deliveryRepository.Delete(ctx, id)
+}
+
+// NewDelivery will create an implementation of postmand.DeliveryService.
+func NewDelivery(deliveryRepository postmand.DeliveryRepository) *Delivery {
+	return &Delivery{deliveryRepository: deliveryRepository}
+}
---
+++ service/delivery_test.go
@@ -0,0 +1,77 @@
+package service
+
+import (
+	"context"
+	"testing"
+
+	"github.com/allisson/postmand"
+	"github.com/allisson/postmand/mocks"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+func TestDelivery(t *testing.T) {
+	ctx := context.Background()
+
+	t.Run("Get", func(t *testing.T) {
+		deliveryRepository := &mocks.DeliveryRepository{}
+		deliveryService := NewDelivery(deliveryRepository)
+		expectedDelivery := &postmand.Delivery{ID: uuid.New()}
+		getOptions := postmand.RepositoryGetOptions{Filters: map[string]interface{}{"id": expectedDelivery.ID}}
+
+		deliveryRepository.On("Get", mock.Anything, getOptions).Return(expectedDelivery, nil)
+		delivery, err := deliveryService.Get(ctx, getOptions)
+		assert.Nil(t, err)
+		assert.Equal(t, expectedDelivery, delivery)
+		deliveryRepository.AssertExpectations(t)
+	})
+
+	t.Run("List", func(t *testing.T) {
+		deliveryRepository := &mocks.DeliveryRepository{}
+		webhookService := NewDelivery(deliveryRepository)
+		expectedDelivery := &postmand.Delivery{ID: uuid.New()}
+		listOptions := postmand.RepositoryListOptions{Filters: map[string]interface{}{"id": expectedDelivery.ID}, Limit: 1, Offset: 0}
+
+		deliveryRepository.On("List", mock.Anything, listOptions).Return([]*postmand.Delivery{expectedDelivery}, nil)
+		webhooks, err := webhookService.List(ctx, listOptions)
+		assert.Nil(t, err)
+		assert.Equal(t, expectedDelivery, webhooks[0])
+		deliveryRepository.AssertExpectations(t)
+	})
+
+	t.Run("Create", func(t *testing.T) {
+		deliveryRepository := &mocks.DeliveryRepository{}
+		webhookService := NewDelivery(deliveryRepository)
+		delivery := &postmand.Delivery{ID: uuid.New()}
+
+		deliveryRepository.On("Create", mock.Anything, delivery).Return(nil)
+		err := webhookService.Create(ctx, delivery)
+		assert.Nil(t, err)
+		deliveryRepository.AssertExpectations(t)
+	})
+
+	t.Run("Update", func(t *testing.T) {
+		deliveryRepository := &mocks.DeliveryRepository{}
+		webhookService := NewDelivery(deliveryRepository)
+		delivery := &postmand.Delivery{ID: uuid.New()}
+
+		getOptions := postmand.RepositoryGetOptions{Filters: map[string]interface{}{"id": delivery.ID}}
+		deliveryRepository.On("Get", mock.Anything, getOptions).Return(delivery, nil)
+		deliveryRepository.On("Update", mock.Anything, delivery).Return(nil)
+		err := webhookService.Update(ctx, delivery)
+		assert.Nil(t, err)
+		deliveryRepository.AssertExpectations(t)
+	})
+
+	t.Run("Delete", func(t *testing.T) {
+		deliveryRepository := &mocks.DeliveryRepository{}
+		webhookService := NewDelivery(deliveryRepository)
+		delivery := &postmand.Delivery{ID: uuid.New()}
+
+		deliveryRepository.On("Delete", mock.Anything, delivery.ID).Return(nil)
+		err := webhookService.Delete(ctx, delivery.ID)
+		assert.Nil(t, err)
+		deliveryRepository.AssertExpectations(t)
+	})
+}
